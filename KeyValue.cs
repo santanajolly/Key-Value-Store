4d6875a74c27eb31a5e6cd22813734a830e03608
Santana Jolly <santanaharris@gmail.com>
2019-09-11 08:49:57 -0400

    Add project files.

---
+++ KeyValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Key_Value_Store
+{
+    struct KeyValue
+    {
+        public string Key { get; }
+        public object Value { get; }
+
+        public KeyValue(string key, object value)
+        {
+            Key = key;
+            Value = value;
+
+        }
+    }
+}
